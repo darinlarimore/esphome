b5fd240e49e21962b174fa3803eb962d29283979
darinlarimore <darinlarimore@gmail.com>
2025-03-16 20:14:14 -0400

    added fix for reverse mode

--- esphome/components/max7219digit/max7219digit.cpp
+++ esphome/components/max7219digit/max7219digit.cpp
@@ -107,21 +107,23 @@ void MAX7219Component::loop() {
 }
 
 void MAX7219Component::display() {
-  uint8_t pixels[8];
+  uint8_t pixels[this->num_chip_lines_][8];
   // Run this loop for every MAX CHIP (GRID OF 64 leds)
   // Run this routine for the rows of every chip 8x row 0 top to 7 bottom
-  // Fill the pixel parameter with display data
+  // Fill the pixel parameter with diplay data
   // Send the data to the chip
-  for (uint8_t chip = 0; chip < this->num_chips_ / this->num_chip_lines_; chip++) {
-    for (uint8_t chip_line = 0; chip_line < this->num_chip_lines_; chip_line++) {
-      for (uint8_t j = 0; j < 8; j++) {
-        pixels[j] = this->max_displaybuffer_[chip_line][(this->num_chips_ / this->num_chip_lines_ - chip - 1) * 8 + j];
+  for (uint8_t i = 0; i < this->num_chips_ / this->num_chip_lines_ ; i++) {
+    for (uint8_t j = 0; j < 8; j++) {
+      for (uint8_t chip_line = 0; chip_line < this->num_chip_lines_; chip_line++){
+        if (this->reverse_) {
+          pixels[chip_line][j] = this->max_displaybuffer_[chip_line][(this->num_chips_ / this->num_chip_lines_ - i - 1) * 8 + j];
+        } else {
+          pixels[chip_line][j] = this->max_displaybuffer_[chip_line][i * 8 + j];
+        }
       }
-      if (chip_line % 2 != 0 && this->chip_lines_style_ == ChipLinesStyle::SNAKE)
-        this->orientation_ = orientation_180_();
-      this->send64pixels(chip_line * this->num_chips_ / this->num_chip_lines_ + chip, pixels);
-      if (chip_line % 2 != 0 && this->chip_lines_style_ == ChipLinesStyle::SNAKE)
-        this->orientation_ = orientation_180_();
+    }
+    for (uint8_t chip_line = 0; chip_line < this->num_chip_lines_; chip_line++){
+      this->send64pixels(chip_line * this->num_chips_ / this->num_chip_lines_ + i, pixels[chip_line]);
     }
   }
 }
@@ -244,88 +246,48 @@ void MAX7219Component::send_char(uint8_t chip, uint8_t data) {
 // send one character (data) to position (chip)
 
 void MAX7219Component::send64pixels(uint8_t chip, const uint8_t pixels[8]) {
-  if (!this->reverse_) {
-    for (uint8_t col = 0; col < 8; col++) {
-      this->enable();
-      for (uint8_t i = 0; i < chip; i++) {
-        this->send_byte_(MAX7219_REGISTER_NOOP, MAX7219_REGISTER_NOOP);
-      }
-      uint8_t b = 0;
-      if (this->orientation_ == 0) {
-        for (uint8_t i = 0; i < 8; i++) {
-          if (this->flip_x_) {
-            b |= ((pixels[i] >> col) & 1) << i;
-          } else {
-            b |= ((pixels[i] >> col) & 1) << (7 - i);
-          }
-        }
-      } else if (this->orientation_ == 1) {
-        b = pixels[col];
-      } else if (this->orientation_ == 2) {
-        for (uint8_t i = 0; i < 8; i++) {
-          if (this->flip_x_) {
-            b |= ((pixels[i] >> (7 - col)) & 1) << (7 - i);
-          } else {
-            b |= ((pixels[i] >> (7 - col)) & 1) << i;
-          }
-        }
-      } else {
-        for (uint8_t i = 0; i < 8; i++) {
-          b |= ((pixels[7 - col] >> i) & 1) << (7 - i);
-        }
-      }
-      if (this->invert_) {
-        this->send_byte_(col + 1, ~b);
-      } else {
-        this->send_byte_(col + 1, b);
-      }
-      for (int i = 0; i < this->num_chips_ - chip - 1; i++) {
-        this->send_byte_(MAX7219_REGISTER_NOOP, MAX7219_REGISTER_NOOP);
-      }
-      this->disable();
+  for (uint8_t col = 0; col < 8; col++) {  // RUN THIS LOOP 8 times until column is 7
+    this->enable();                        // start sending by enabling SPI
+    for (uint8_t i = 0; i < chip; i++) {   // send extra NOPs to push the pixels out to extra displays
+      this->send_byte_(MAX7219_REGISTER_NOOP,
+                       MAX7219_REGISTER_NOOP);  // run this loop unit the matching chip is reached
     }
-  } else {
-    for (int col = 7; col >= 0; col--) {
-      this->enable();
-      for (uint8_t i = 0; i < chip; i++) {
-        this->send_byte_(MAX7219_REGISTER_NOOP, MAX7219_REGISTER_NOOP);
-      }
-      uint8_t b = 0;
-      if (this->orientation_ == 0) {
-        for (uint8_t i = 0; i < 8; i++) {
-          if (this->flip_x_) {
-            b |= ((pixels[i] >> col) & 1) << i;
-          } else {
-            b |= ((pixels[i] >> col) & 1) << (7 - i);
-          }
-        }
-      } else if (this->orientation_ == 1) {
-        b = pixels[col];
-      } else if (this->orientation_ == 2) {
-        for (uint8_t i = 0; i < 8; i++) {
-          if (this->flip_x_) {
-            b |= ((pixels[i] >> (7 - col)) & 1) << (7 - i);
-          } else {
-            b |= ((pixels[i] >> (7 - col)) & 1) << i;
-          }
-        }
-      } else {
-        for (uint8_t i = 0; i < 8; i++) {
-          b |= ((pixels[7 - col] >> i) & 1) << (7 - i);
+    uint8_t b = 0;  // rotate pixels 90 degrees -- set byte to 0
+    if (this->orientation_ == 0) {
+      for (uint8_t i = 0; i < 8; i++) {
+        // run this loop 8 times for all the pixels[8] received
+        if (this->flip_x_) {
+          b |= ((pixels[i] >> col) & 1) << i;  // change the column bits into row bits
+        } else {
+          b |= ((pixels[i] >> col) & 1) << (7 - i);  // change the column bits into row bits
         }
       }
-      if (this->invert_) {
-        this->send_byte_(col + 1, ~b);
-      } else {
-        this->send_byte_(col + 1, b);
+    } else if (this->orientation_ == 1) {
+      b = pixels[col];
+    } else if (this->orientation_ == 2) {
+      for (uint8_t i = 0; i < 8; i++) {
+        if (this->flip_x_) {
+          b |= ((pixels[i] >> (7 - col)) & 1) << (7 - i);
+        } else {
+          b |= ((pixels[i] >> (7 - col)) & 1) << i;
+        }
       }
-      for (int i = 0; i < this->num_chips_ - chip - 1; i++) {
-        this->send_byte_(MAX7219_REGISTER_NOOP, MAX7219_REGISTER_NOOP);
+    } else {
+      for (uint8_t i = 0; i < 8; i++) {
+        b |= ((pixels[7 - col] >> i) & 1) << (7 - i);
       }
-      this->disable();
     }
-  }
-}
+    // send this byte to display at selected chip
+    if (this->invert_) {
+      this->send_byte_(col + 1, ~b);
+    } else {
+      this->send_byte_(col + 1, b);
+    }
+    for (int i = 0; i < this->num_chips_ - chip - 1; i++)  // end with enough NOPs so later chips don't update
+      this->send_byte_(MAX7219_REGISTER_NOOP, MAX7219_REGISTER_NOOP);
+    this->disable();  // all done disable SPI
+  }                   // end of for each column
+}  // end of send64pixels
 
 uint8_t MAX7219Component::printdigit(const char *str) { return this->printdigit(0, str); }
 
